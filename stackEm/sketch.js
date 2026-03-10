363de665e3df51868a768ea591f2ef79996326dd
Bat67-png <7020180@spsd.sk.ca>
2026-03-10 11:03:31 -0600

    bottom block attempt

--- stackEm/sketch.js
+++ stackEm/sketch.js
@@ -5,71 +5,89 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let state = "right";
-let speed = 5;
+// let state = "right";
+// let speed = 5;
+// let blockArray = [];
+// let w;
+// let h;
+
+// function setup() {
+//   createCanvas(700, 700);
+//   noStroke();
+// }
+
+// function draw() {
+//   background(220);
+//   for (let block of blockArray) {
+//   //  move
+//     if (state === "right") {
+//       block.x += speed;
+//     }
+//     else if (state === "left") {
+//       block.x -= speed;
+//     }
+//     else if (state === "stop") {
+//       block.x += 0;
+//     }
+
+//     fill("black");
+//     rect(block.x, block.y, w, h);
+//   }
+//   spawnBlock();
+// }
+
+
+// function spawnBlock(_w, _h) {
+//   // move
+//   let someBlock = {
+//     x: width - w/2,
+//     y: height - y,
+//     w: _w,
+//     h: _h,
+//   };
+//   blockArray.push(someBlock);
+// }
+
+
 let blockArray = [];
+let x;
+let y;
 let w;
 let h;
+let speed = 1;
+let state = "right";
+
 
 function setup() {
   createCanvas(700, 700);
-  noStroke();
+  w = 80;
+  h = 50;
+  x = width/2 - w/2;
+  y = height - 2*h;
 }
 
 function draw() {
   background(220);
+  spawnBlock();
+  // block();
   for (let block of blockArray) {
-  //  move
+    // movement
     if (state === "right") {
       block.x += speed;
     }
     else if (state === "left") {
       block.x -= speed;
-    }
+    } 
     else if (state === "stop") {
       block.x += 0;
+      fill("green");
+      rect(block.x, block.y, block.w, block.h );
+      noLoop();
     }
-
     fill("black");
-    rect(block.x, block.y, w, h);
+    rect(block.x, block.y - block.h , block.w, block.h);
   }
-  spawnBlock();
 }
-
-
-function spawnBlock(_w, _h) {
-  // move
-  let someBlock = {
-    x: width - w/2,
-    y: height - y,
-    w: _w,
-    h: _h,
-  };
-  blockArray.push(someBlock);
-}
-
-
-
-// let x;
-// let y;
-// let w;
-// let h;
-// let speed = 1;
-// let state = "right";
-
-
-// function setup() {
-//   createCanvas(700, 700);
-//   w = 80;
-//   h = 50;
-//   x = width/2 - w/2;
-//   y = height - h;
-// }
-
-// function draw() {
-//   background(220);
-//   block();
-// }
 
 // function block() {
 //   noStroke();
@@ -84,11 +102,23 @@ function spawnBlock(_w, _h) {
 //   else if (state === "stop") {
 //     x += 0;
 //   }
-//   console.log(state)
+//   console.log(state);
 // }
 
-// function keyPressed() {
-//   if (key === 's') {
-//     state = "stop";
-//   }
-// }
+function keyPressed() {
+  if (key === 's') {
+    state = "stop";
+    fill("green");
+    rect(block.x, block.y - block.h, block.w, block.h);
+  }
+}
+
+function spawnBlock() {
+  let someBlock = {
+    x: width/2 - w/2,
+    y: height - 2*h,
+    w: random(50, 80),
+    h: random(40, 50)
+  };
+  blockArray.push(someBlock);
+}
