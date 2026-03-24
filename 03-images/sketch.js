2c54c62fcff155de36d35c99757d3be3472bc5bb
Bat67-png <7020180@spsd.sk.ca>
2026-03-24 11:03:14 -0600

    chess board

--- 03-images/sketch.js
+++ 03-images/sketch.js
@@ -1,6 +1,9 @@
 // Image Demo
 
 let luigiImg;
+let x;
+let y;
+let speed = 5;
 
 function preload() {
   luigiImg = loadImage("luigi.png");
@@ -9,9 +12,20 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  x = width/2;
+  y = height/2;
 }
 
 function draw() {
   background(220);
-  image(luigiImg, mouseX, mouseY, luigiImg.width*0.25, luigiImg.height*0.25);
+  image(luigiImg, x, y, luigiImg.width*0.25, luigiImg.height*0.25);
+}
+
+function keyPressed() {
+  if (key === "a") {
+    x -= speed;
+  }
+  if (key === "d") {
+    x += speed;
+  }
 }
--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -1,77 +1,80 @@
-// 2D Grid Demo
-//learning 2d arrays
+// Chess puzzle
+// Bat-Erdene Lkhagvasuren
+// Date
+//
+// Extra for Experts:
+// 
 
-//use this if hard coded grid
-// let theGrid = [[1, 0, 1, 0],
-//                [1, 0, 1, 0],
-//                [0, 1, 0, 1],
-//                [0, 0, 1, 1]];
-// const SQUARE_DIMENSION = theGrid.length;
-
-//use this to randomize grid
 let theGrid;
-const SQUARE_DIMENSION = 8;
+const Pawn = 1;
+const WHITE_KING = 2;
+const BLACK_KING = 3;
+const BOARD_DIMENSION = 8;
+const LIVE_CELL = 1;
+const DEAD_CELL = 0;
 let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   if (width > height) {
-    cellSize = height/SQUARE_DIMENSION;
+    cellSize = height/BOARD_DIMENSION;
   }
   else {
-    cellSize = width/SQUARE_DIMENSION;
+    cellSize = width/BOARD_DIMENSION;
   }
-  theGrid = generateRandomGrid(SQUARE_DIMENSION, SQUARE_DIMENSION);
+  theGrid = generateTHeBoard(BOARD_DIMENSION, BOARD_DIMENSION);
 }
 
 function draw() {
   background(220);
-  showGrid();
+  chessBoard();
 }
 
 function mousePressed() {
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
-  // console.log(x, y);
-  toggleCell(x, y);
+  console.log(x, y);
+
+  console.log(theGrid);
 }
 
-function toggleCell(x, y) {
-  if (theGrid[y][x] === 1) {
-    theGrid[y][x] = 0;
-  }
-  else if (theGrid[y][x] === 0) {
-    theGrid[y][x] = 1;
-  }
-}
+// function toggleCell(x, y) {
 
-function showGrid() {
-  for (let y = 0; y < SQUARE_DIMENSION; y++) {
-    for (let x = 0; x < SQUARE_DIMENSION; x++) {
-      if (theGrid[y][x] === 1) {
-        fill("black");
-      }
-      else if (theGrid[y][x] === 0) {
-        fill("white");
-      }
-      square(x * cellSize, y * cellSize, cellSize);
-    }
-  }
-}
+// }
 
-function generateRandomGrid(cols, rows) {
+
+function generateTHeBoard(cols, rows) {
   let newGrid = [];
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
-      if (random(100) < 50) {
-        newGrid[y].push(0);
+      if (y === 1 || 6) {
+        newGrid[y].push(Pawn);
       }
       else {
-        newGrid[y].push(1);
+        newGrid[y].push(0);
       }
+      
     }
   }
   return newGrid;
+}
+
+
+function chessBoard() {
+  let isWhite = true;
+  for (let x = 0; x < BOARD_DIMENSION; x++) {
+    for (let y = 0; y < BOARD_DIMENSION; y++) {
+      if (isWhite) {
+        fill("white");
+      }
+      else {
+        fill("black");
+      }
+      square(x*cellSize, y*cellSize, cellSize);
+      isWhite = !isWhite;
+    }
+    isWhite = !isWhite;
+  }
 }
