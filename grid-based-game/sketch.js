bd18b3514d90b969cf45c24bcd033458fa91768d
Bat67-png <7020180@spsd.sk.ca>
2026-03-31 10:23:23 -0600

    pieces are added

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -5,15 +5,21 @@
 // Extra for Experts:
 // 
 const EMPTY = 0;
-const PAWN = 1;
-const WHITE_KING = 2;
+const BLACK_PAWN = 1;
+const WHITE_PAWN = 2;
 const BLACK_KING = 3;
 const BOARD_DIMENSION = 8;
 let theGrid;
 let cols;
 let rows;
 let cellSize;
+
+// pieces declared
 let blackKingImg;
+let blackPawnImg;
+let whitePawnImg;
+
+// pieces coordinates
 let theBlackKing = {
   x: 7,
   y: 0,
@@ -22,9 +28,61 @@ let theWhiteKing = {
   x: 3,
   y: 2
 };
+let blackPawn1 = {
+  x: 7,
+  y: 1
+};
+
+let blackPawn2 = {
+  x: 6,
+  y: 1
+};
+
+let blackPawn3 = {
+  x: 2,
+  y: 1
+};
+
+let blackPawn4 = {
+  x: 1,
+  y: 1
+};
+
+let blackPawn5 = {
+  x: 0,
+  y: 2
+};
+
+let whitePawn1 = {
+  x: 6,
+  y: 6
+};
+
+let whitePawn2 = {
+  x: 5,
+  y: 6
+};
+
+let whitePawn3 = {
+  x: 2,
+  y: 6
+};
+
+let whitePawn4 = {
+  x: 0,
+  y: 6
+};
+
+let whitePawn5 = {
+  x: 2,
+  y: 5
+};
+
 
 function preload() {
-  blackKingImg = loadImage("black.king.png");
+  blackKingImg = loadImage("black.king.svg");
+  blackPawnImg = loadImage("black.pawn.svg");
+  whitePawnImg = loadImage("white.pawn.svg");
 }
 
 function setup() {
@@ -40,6 +98,19 @@ function setup() {
   theGrid = generateTHeBoard(BOARD_DIMENSION, BOARD_DIMENSION);
 
   theGrid[theBlackKing.y][theBlackKing.x] = BLACK_KING;
+  theGrid[blackPawn1.y][blackPawn1.x] = BLACK_PAWN;
+  theGrid[blackPawn2.y][blackPawn2.x] = BLACK_PAWN;
+  theGrid[blackPawn3.y][blackPawn3.x] = BLACK_PAWN;
+  theGrid[blackPawn4.y][blackPawn4.x] = BLACK_PAWN;
+  theGrid[blackPawn5.y][blackPawn5.x] = BLACK_PAWN;
+
+  theGrid[whitePawn1.y][whitePawn1.x] = WHITE_PAWN;
+  theGrid[whitePawn2.y][whitePawn2.x] = WHITE_PAWN;
+  theGrid[whitePawn3.y][whitePawn3.x] = WHITE_PAWN;
+  theGrid[whitePawn4.y][whitePawn4.x] = WHITE_PAWN;
+  theGrid[whitePawn5.y][whitePawn5.x] = WHITE_PAWN;
+
+
 }
 
 function draw() {
@@ -47,11 +118,18 @@ function draw() {
   chessBoard();
 }
 
+// Display the chess pieces on the board
 function displayPieces() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (theGrid[y][x] === BLACK_KING) {
         image(blackKingImg, x*cellSize, y*cellSize, cellSize, cellSize);
+      }
+      if (theGrid[y][x] === BLACK_PAWN) {
+        image(blackPawnImg, x*cellSize, y*cellSize, cellSize, cellSize);
+      }
+      if (theGrid[y][x] === WHITE_PAWN) {
+        image(whitePawnImg, x*cellSize, y*cellSize, cellSize, cellSize);
       }
     }
   }
@@ -91,8 +169,9 @@ function chessBoard() {
         fill("white");
       }
       if (!isWhite) {
-        fill("black");
+        fill("darkgray");
       }
+      noStroke();
       square(x*cellSize, y*cellSize, cellSize);
       isWhite = !isWhite;
     }
