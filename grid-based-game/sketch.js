8e6809b47d61d0c9e25ea7087ae4fdb422b6e8b3
Bat67-png <7020180@spsd.sk.ca>
2026-03-31 11:02:56 -0600

    some attempt to create different puzzles

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -18,74 +18,77 @@ let cellSize;
 let blackKingImg;
 let blackPawnImg;
 let whitePawnImg;
+let puzzle;
 
 // pieces coordinates
-let theBlackKing = {
-  x: 7,
-  y: 0,
-};
-let theWhiteKing = {
-  x: 3,
-  y: 2
-};
-let blackPawn1 = {
-  x: 7,
-  y: 1
-};
+// let theBlackKing = {
+//   x: 7,
+//   y: 0,
+// };
+// let theWhiteKing = {
+//   x: 3,
+//   y: 2
+// };
+// let blackPawn1 = {
+//   x: 7,
+//   y: 1
+// };
 
-let blackPawn2 = {
-  x: 6,
-  y: 1
-};
+// let blackPawn2 = {
+//   x: 6,
+//   y: 1
+// };
 
-let blackPawn3 = {
-  x: 2,
-  y: 1
-};
+// let blackPawn3 = {
+//   x: 2,
+//   y: 1
+// };
 
-let blackPawn4 = {
-  x: 1,
-  y: 1
-};
+// let blackPawn4 = {
+//   x: 1,
+//   y: 1
+// };
 
-let blackPawn5 = {
-  x: 0,
-  y: 2
-};
+// let blackPawn5 = {
+//   x: 0,
+//   y: 2
+// };
 
-let whitePawn1 = {
-  x: 6,
-  y: 6
-};
+// let whitePawn1 = {
+//   x: 6,
+//   y: 6
+// };
 
-let whitePawn2 = {
-  x: 5,
-  y: 6
-};
+// let whitePawn2 = {
+//   x: 5,
+//   y: 6
+// };
 
-let whitePawn3 = {
-  x: 2,
-  y: 6
-};
+// let whitePawn3 = {
+//   x: 2,
+//   y: 6
+// };
 
-let whitePawn4 = {
-  x: 0,
-  y: 6
-};
+// let whitePawn4 = {
+//   x: 0,
+//   y: 6
+// };
 
-let whitePawn5 = {
-  x: 2,
-  y: 5
-};
+// let whitePawn5 = {
+//   x: 2,
+//   y: 5
+// };
 
 
 function preload() {
   blackKingImg = loadImage("black.king.svg");
   blackPawnImg = loadImage("black.pawn.svg");
   whitePawnImg = loadImage("white.pawn.svg");
+  puzzle = loadStrings("assets/1.puzzle");
 }
 
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
   if (width > height) {
     cellSize = height/BOARD_DIMENSION;
@@ -97,18 +100,18 @@ function setup() {
   cols = 8;
   theGrid = generateTHeBoard(BOARD_DIMENSION, BOARD_DIMENSION);
 
-  theGrid[theBlackKing.y][theBlackKing.x] = BLACK_KING;
-  theGrid[blackPawn1.y][blackPawn1.x] = BLACK_PAWN;
-  theGrid[blackPawn2.y][blackPawn2.x] = BLACK_PAWN;
-  theGrid[blackPawn3.y][blackPawn3.x] = BLACK_PAWN;
-  theGrid[blackPawn4.y][blackPawn4.x] = BLACK_PAWN;
-  theGrid[blackPawn5.y][blackPawn5.x] = BLACK_PAWN;
+  // theGrid[theBlackKing.y][theBlackKing.x] = BLACK_KING;
+  // theGrid[blackPawn1.y][blackPawn1.x] = BLACK_PAWN;
+  // theGrid[blackPawn2.y][blackPawn2.x] = BLACK_PAWN;
+  // theGrid[blackPawn3.y][blackPawn3.x] = BLACK_PAWN;
+  // theGrid[blackPawn4.y][blackPawn4.x] = BLACK_PAWN;
+  // theGrid[blackPawn5.y][blackPawn5.x] = BLACK_PAWN;
 
-  theGrid[whitePawn1.y][whitePawn1.x] = WHITE_PAWN;
-  theGrid[whitePawn2.y][whitePawn2.x] = WHITE_PAWN;
-  theGrid[whitePawn3.y][whitePawn3.x] = WHITE_PAWN;
-  theGrid[whitePawn4.y][whitePawn4.x] = WHITE_PAWN;
-  theGrid[whitePawn5.y][whitePawn5.x] = WHITE_PAWN;
+  // theGrid[whitePawn1.y][whitePawn1.x] = WHITE_PAWN;
+  // theGrid[whitePawn2.y][whitePawn2.x] = WHITE_PAWN;
+  // theGrid[whitePawn3.y][whitePawn3.x] = WHITE_PAWN;
+  // theGrid[whitePawn4.y][whitePawn4.x] = WHITE_PAWN;
+  // theGrid[whitePawn5.y][whitePawn5.x] = WHITE_PAWN;
 
 
 }
@@ -159,6 +162,12 @@ function generateTHeBoard(cols, rows) {
   return newGrid;
 }
 
+function keyPressed() {
+  if (key === "p") {
+    theGrid = puzzle;
+    displayPieces();
+  }
+}
 
 // Creates the visual board 
 function chessBoard() {
