84b8d772ebcc4392a8e4f8e51608fe8ebe7102d5
Bat67-png <7020180@spsd.sk.ca>
2026-04-12 15:41:49 -0600

    Click based movement system, string splitter, and added game states.

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -1,20 +1,21 @@
 // Chess puzzle
 // Bat-Erdene Lkhagvasuren
-// Date
+// 2026-04-12
 //
 // Extra for Experts:
-// 
+// Created a click based movement system 
+// Built a string splitter that takes in data from an external file
 
 const EMPTY = "0";
 const BOARD_DIMENSION = 8;
-let chessMoveGrid;
+let state = "board"
 let theGrid;
 let cols;
 let rows;
 let cellSize;
 let puzzle;
 let board;
-let selected = null; // 
+let selected = null
 
 // White pieces
 const WHITE_PAWN = "2";
@@ -39,7 +40,6 @@ let blackKnightImg;
 let blackRookImg;
 let blackBishopImg;
 let blackQueenImg;
-
 let whiteKingImg;
 let whitePawnImg;
 let whiteKnightImg;
@@ -47,19 +47,15 @@ let whiteRookImg;
 let whiteBishopImg;
 let whiteQueenImg;
 
-
-
-
-
-
+// Preloads all the pictures and the puzzle
 function preload() {
+  // Chess pieces
   blackKingImg = loadImage("assets/black.king.svg");
   blackPawnImg = loadImage("assets/black.pawn.svg");
   blackBishopImg = loadImage("assets/blackBishop.svg");
   blackQueenImg = loadImage("assets/blackQueen.svg");
   blackRookImg = loadImage("assets/blackRook.svg");
   blackKnightImg = loadImage("assets/blackKnight.svg");
-
   whitePawnImg = loadImage("assets/white.pawn.svg");
   whiteKnightImg = loadImage("assets/white.knight.svg");
   whiteBishopImg = loadImage("assets/whiteBishop.svg");
@@ -67,12 +63,12 @@ function preload() {
   whiteQueenImg = loadImage("assets/whiteQueen.svg");
   whiteRookImg = loadImage("assets/whiteRook.svg");
 
+  // Puzzle and the main chess board
   puzzle = loadStrings("assets/1.puzzle");
   board = loadStrings("assets/2.board");
 }
 
 function setup() {
-
   createCanvas(windowWidth, windowHeight);
   if (width > height) {
     cellSize = height/BOARD_DIMENSION;
@@ -88,66 +84,110 @@ function setup() {
 function draw() {
   background(220);
   chessBoard();
+
+  // Function that shows whether you got the puzzle right or wrong
+  if (state === "win") {
+    fill("Darkblue")
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    text('You won!', width/2, height/2);
+  }
+  if (state === "lose") {
+    fill("Darkblue")
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    text('Try again!', width/2, height/2);
+  }
 }
 
 function mousePressed() {
-  let x = Math.floor(mouseX/CELL_SIZE);
-  let y = Math.floor(mouseY/CELL_SIZE);
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
   
+  if (selected === null) {
+    // Selects the piece that you want to move
+    if (theGrid[y][x] !== EMPTY) {
+      selected = {x: x, y: y};
+    }
+  }
+  else {
+    // The movement of the selected piece
+    let piece = theGrid[selected.y][selected.x];
+    theGrid[y][x] = piece;
+    theGrid[selected.y][selected.x] = EMPTY;
+    selected = null;
+    puzzleOver();
+  }
+
+
 }
 
 // Displays the chess pieces on the board
 function displayPieces() {
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      if (theGrid[y][x] === BLACK_KING) {
-        image(blackKingImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === BLACK_PAWN) {
-        image(blackPawnImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === BLACK_BISHOP) {
-        image(blackBishopImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === BLACK_KNIGHT) {
-        image(blackKnightImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === BLACK_ROOK) {
-        image(blackRookImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === BLACK_QUEEN) {
-        image(blackQueenImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
+  if (state === "puzzle" || state === "board") {
 
-      if (theGrid[y][x] === WHITE_PAWN) {
-        image(whitePawnImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === WHITE_KNIGHT) {
-        image(whiteKnightImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === WHITE_BISHOP) {
-        image(whiteBishopImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === WHITE_ROOK) {
-        image(whiteRookImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === WHITE_QUEEN) {
-        image(whiteQueenImg, x*cellSize, y*cellSize, cellSize, cellSize);
-      }
-      if (theGrid[y][x] === WHITE_KING) {
-        image(whiteKingImg, x*cellSize, y*cellSize, cellSize, cellSize);
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        // Black pieces
+        if (theGrid[y][x] === BLACK_KING) {
+          image(blackKingImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === BLACK_PAWN) {
+          image(blackPawnImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === BLACK_BISHOP) {
+          image(blackBishopImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === BLACK_KNIGHT) {
+          image(blackKnightImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === BLACK_ROOK) {
+          image(blackRookImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === BLACK_QUEEN) {
+          image(blackQueenImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        // White pieces
+        if (theGrid[y][x] === WHITE_PAWN) {
+          image(whitePawnImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === WHITE_KNIGHT) {
+          image(whiteKnightImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === WHITE_BISHOP) {
+          image(whiteBishopImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === WHITE_ROOK) {
+          image(whiteRookImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === WHITE_QUEEN) {
+          image(whiteQueenImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
+        if (theGrid[y][x] === WHITE_KING) {
+          image(whiteKingImg, x*cellSize, y*cellSize, cellSize, cellSize);
+        }
       }
     }
   }
 }
 
+// Switches into the puzzle when the key P is pressed
 function keyPressed() {
   if (key === "p") {
     theGrid = loadPuzzle(puzzle);
+    state = "puzzle";
   }
+  if (key === "b") {
+    theGrid = loadPuzzle(board);
+    state = "board"
+  }
+  if (key === "w") {
+    state = "win"
+  }
+
 }
 
-// uses the external assets to create the chess board
+// Function that uses the external assets to create the chess board
 function loadPuzzle(thePuzzle) {
   let newGrid = [];
 
@@ -165,20 +205,34 @@ function loadPuzzle(thePuzzle) {
 
 // Creates the visual board 
 function chessBoard() {
-  let isWhite = true;
-  for (let x = 0; x < BOARD_DIMENSION; x++) {
-    for (let y = 0; y < BOARD_DIMENSION; y++) {
-      if (isWhite) {
-        fill("white");
+  if (state === "board" || state === "puzzle") {
+    let isWhite = true;
+    for (let x = 0; x < BOARD_DIMENSION; x++) {
+      for (let y = 0; y < BOARD_DIMENSION; y++) {
+        if (isWhite) {
+          fill("white");
+        }
+        if (!isWhite) {
+          fill("darkgray");
+        }
+        noStroke();
+        square(x*cellSize, y*cellSize, cellSize);
+        isWhite = !isWhite;
       }
-      if (!isWhite) {
-        fill("darkgray");
-      }
-      noStroke();
-      square(x*cellSize, y*cellSize, cellSize);
       isWhite = !isWhite;
     }
-    isWhite = !isWhite;
+    displayPieces();
   }
-  displayPieces();
-} 
+}
+
+// Function that ends the puzzle after making a move in the puzzle
+function puzzleOver() {
+  if (state === "puzzle") {
+    if (theGrid[4][2] === WHITE_KNIGHT) {
+      state = "win"
+    }
+    else {
+      state = "lose"
+    }
+  }
+}
